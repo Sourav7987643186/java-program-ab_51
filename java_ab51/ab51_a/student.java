904eb9a4f221d236f6aa2daa17ac8fa827e59507
Sourav7987643186 <bhattacharyasourav215@gmail.com>
2025-03-25 21:07:33 +0530

    4th commit

---
+++ java_ab51/ab51_a/student.java
@@ -0,0 +1,20 @@
+package ab51_a;
+
+public class student {
+
+	public static void main(String[] args) 
+	{
+		teacher.
+		
+		/* we can not access any private method inside package
+		 * 
+		public             true
+		protected          true
+		package/ default   true
+		private            false
+		
+*/
+
+	}
+
+}
---
+++ java_ab51/ab51_a/teacher.java
@@ -0,0 +1,26 @@
+package ab51_a;
+// Access specifier
+
+public class teacher 
+{
+	public static void add()
+	{
+		
+	}
+	private static void sub()
+	{
+		
+	}
+	protected static void mul()
+	{
+		
+	}
+	static void div()
+	{
+		
+	}
+	{
+	
+	}
+
+}
